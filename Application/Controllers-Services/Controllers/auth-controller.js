506d04a4901928570595bab84113939e6bdd3799
NightFury00X <surendra136721@gmail.com>
2017-11-15 11:19:46 +0530

    fix issue related to signup api to parse form data to json data.

--- Application/Controllers-Services/Controllers/auth-controller.js
+++ Application/Controllers-Services/Controllers/auth-controller.js
@@ -4,23 +4,13 @@ let AuthService = require('../Services/auth-service'),
 // The authentication controller.
 let AuthController = {};
 
-let multer = require('multer'),
-    upload = multer({dest: 'Public/Profile'});
-
 // Register a user.
 AuthController.signUp = async (req, res, next) => {
-    // let userData = req.body.details.split('[').join('{');
-    // userData = userData.split(']').join('}');
-    // let registrationData = userData.split(']').join('}');
-    // console.log('ROW Data: ', registrationData);
-    // console.log('Data: ', JSON.parse(registrationData));
-    
-    console.log('Requested file: ', req.body);
     try {
-        let result = await AuthService.signup(JSON.parse(req.body.details), req.files);
+        let registrationData = JSON.parse(req.body.details);
+        let result = await AuthService.signup(registrationData, req.files);
         responseHelper.setSuccessResponse({message: result}, res, 201);
     } catch (error) {
-        console.log('Error: ', error);
         next(error);
     }
 };
