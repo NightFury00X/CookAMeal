7037fbd6248f289d9bdc424a50be60d1e36db348
NightFury00X <surendra136721@gmail.com>
2017-11-16 10:45:19 +0530

    fix file extension check.

--- Configurations/Helpers/file-uploader.js
+++ Configurations/Helpers/file-uploader.js
@@ -1,26 +1,31 @@
 let path = require('path');
 let FileUploder = {};
 
+let fileExtension = ['.jpg', '.jpeg', '.png', '.'];
+
 //Upload profile image
 FileUploder.UploadProfile = function (file) {
     try {
         return new Promise((resolve, reject) => {
             let filePath = path.join(__dirname, '../../Public/Profile/');
+            console.log('profile File Path: ', filePath);
             let extension = path.extname(file.name);
-            if (extension !== '.jpg') {
-                return reject(new Error('Invalid file format.'));
+            if (fileExtension.indexOf(extension) < 0) {
+                return reject(new Error('Invalid file format [' + extension + '].'));
             }
             let fileName = (new Date).valueOf() + '-' + file.name;
             file.mv(filePath + fileName, function (error) {
                 if (error) {
                     return reject(error);
                 } else {
-                    return resolve({file:{
-                        name: file.name,
-                        encoding: file.encoding,
-                        mimetype: file.mimetype,
-                        imageurl:  filePath + fileName
-                    }});
+                    return resolve({
+                        file: {
+                            name: file.name,
+                            encoding: file.encoding,
+                            mimetype: file.mimetype,
+                            imageurl: filePath + fileName
+                        }
+                    });
                 }
             });
         });
@@ -34,21 +39,24 @@ FileUploder.UploadDoc = function (file) {
     try {
         return new Promise((resolve, reject) => {
             let filePath = path.join(__dirname, '../../Public/Documents/');
+            console.log('Doc File Path: ', filePath);
             let extension = path.extname(file.name);
-            if (extension !== '.jpg') {
-                return reject(new Error('Invalid file format.'));
+            if (fileExtension.indexOf(extension) < 0) {
+                return reject(new Error('Invalid file format [' + extension + '].'));
             }
             let fileName = (new Date).valueOf() + '-' + file.name;
             file.mv(filePath + fileName, function (error) {
                 if (error) {
                     return reject(error);
                 } else {
-                    return resolve({file:{
-                        name: file.name,
-                        encoding: file.encoding,
-                        mimetype: file.mimetype,
-                        imageurl:  filePath + fileName
-                    }});
+                    return resolve({
+                        file: {
+                            name: file.name,
+                            encoding: file.encoding,
+                            mimetype: file.mimetype,
+                            imageurl: filePath + fileName
+                        }
+                    });
                 }
             });
         });
