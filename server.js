fa64b7decaa9e54915dc2fcfc345658d7a8441ac
NightFury00X <surendra136721@gmail.com>
2017-11-20 11:42:40 +0530

    Fixed Allow-Access-Control-Origin

--- server.js
+++ server.js
@@ -54,8 +54,10 @@ app.use(passport.initialize());
 // Hook the passport JWT strategy.
 hookJWTStrategy(passport);
 
+
+
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', ['http://localhost:8081', 'http://localhost:8100', 'https://cook-a-meal-testing.herokuapp.com']);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
     res.header('Access-Control-Allow-Credentials', 'true');
