caaa43f148c3766a60314558b2882f046140ef0a
NightFury00X <surendra136721@gmail.com>
2017-11-28 17:10:20 +0530

    Fixed API Routing 404 Error.

--- server.js
+++ server.js
@@ -70,7 +70,16 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
     res.header('Access-Control-Allow-Credentials', 'true');
-    next();
+    
+    //intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+        //respond with 200
+        res.send(200);
+    }
+    else {
+        //move on
+        next();
+    }
 });
 
 // Helmet
