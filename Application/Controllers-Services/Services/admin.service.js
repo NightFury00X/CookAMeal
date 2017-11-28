a44ded7abde8538efe534e3e7a3acf87fd797429
NightFury00X <surendra136721@gmail.com>
2017-11-28 12:26:34 +0530

    Fixed Application/json type data

--- Application/Controllers-Services/Services/admin.service.js
+++ Application/Controllers-Services/Services/admin.service.js
@@ -8,22 +8,20 @@ AdminService.prototype.Add = async (userid, category, files) => {
     const trans = await db.sequelize.transaction();
     try {
         let categoryData = await db.Category.create(category, {transaction: trans});
-
-        let CategoryData;
-
-        // let mediaObject;
+    
+        let CategoryDataMedia;
         if (files && files.category) {
             let CategoryImage = files.category[0];
             CategoryImage.category_id = categoryData.id;
             CategoryImage.imageurl = CommonConfig.FILE_LOCATIONS.CATEGORY + CategoryImage.filename;
             CategoryImage.object_type = CommonConfig.OBJECT_TYPE.CATEGORY;
-            CategoryData = await db.MediaObject.create(CategoryImage, {transaction: trans});
+            CategoryDataMedia = await db.MediaObject.create(CategoryImage, {transaction: trans});
         }
 
         // commit transaction
         await trans.commit();
 
-        return {Category: categoryData, MediaObject: CategoryData};
+        return {Category: categoryData, MediaObject: CategoryDataMedia};
     } catch (error) {
         // rollback transaction
         await trans.rollback();
