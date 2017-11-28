01306161b82da9df16aab168f0e88e42f78c68dc
NightFury00X <surendra136721@gmail.com>
2017-11-28 12:10:34 +0530

    Added 1. Get all categories list. 2. Get category by category Id.

--- Configurations/middlewares/request-checker.js
+++ Configurations/middlewares/request-checker.js
@@ -4,6 +4,7 @@ let RequestMethods = {},
 
 RequestMethods.CheckAuthorizationHeader = function (req, res, next) {
     let isAuth = req.get('Authorization');
+    console.log('Content-Type: ', isAuth);
     if (!isAuth)
         return responseHelper.setErrorResponse({message: 'Header not present in the request. '}, res, CommonConfig.STATUS_CODE.BAD_REQUEST);
     next();
@@ -12,12 +13,14 @@ RequestMethods.CheckAuthorizationHeader = function (req, res, next) {
 RequestMethods.CheckContentType = {
     ApplicationJsonData: function (req, res, next) {
         let isJsonData = req.get('Content-Type');
+        console.log('Content-Type: ', isJsonData);
         if (isJsonData !== 'application/json')
             return responseHelper.setErrorResponse({message: 'Invalid Content Type. Content-Type: applicaiotn/json required!'}, res, CommonConfig.STATUS_CODE.BAD_REQUEST);
         next();
     },
     ApplicationFormData: function (req, res, next) {
         let isMultiPart = req.get('Content-Type');
+        console.log('Content-Type: ', isMultiPart);
         if (isMultiPart.split(';')[0] !== 'multipart/form-data')
             return responseHelper.setErrorResponse({message: 'Invalid Content Type. Content-Type: multipart/form-data required!'}, res, CommonConfig.STATUS_CODE.BAD_REQUEST);
         next();
