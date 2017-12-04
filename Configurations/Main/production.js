4628cb7111eae2637324d40f8d10d8a0ceba0fa5
NightFury00X <surendra136721@gmail.com>
2017-12-04 10:50:35 +0530

    Changed password functionality added.

--- Configurations/Main/production.js
+++ Configurations/Main/production.js
@@ -57,7 +57,7 @@ exports.CONFIG = {
     },
     EMAIL_OPTIONS: {
         host: 'smtp.gmail.com',
-        port: 587,
+        port: 25,
         auth: {
             user: 'curacall2015@gmail.com',
             pass: 'Curacall_2015'
