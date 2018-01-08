2b0160228a1530d0964a57b954b8ba725673ba66
NightFury00X <surendra136721@gmail.com>
2018-01-08 18:12:57 +0530

    Remove Sequelize logging.

--- application/schemas/schema.js
+++ application/schemas/schema.js
@@ -75,11 +75,11 @@ module.exports = {
             comments: Joi.string().required()
         }),
         Order: Joi.object().keys({
-            orderType: Joi.string().required(),
+            orderType: Joi.number().required().allow(0, 1),
             spiceLevel: Joi.string().required().allow('Mild', 'Medium', 'Hot'),
-            orderServings: Joi.string().required(),
+            orderServings: Joi.number().required(),
             specialInstruction: Joi.string().required(),
-            deliveryType: Joi.string().required(),
+            deliveryType: Joi.number().required(),
             deliveryFee: Joi.string().required(),
             pickUpTime: Joi.string().required(),
             taxes: Joi.string().required(),
