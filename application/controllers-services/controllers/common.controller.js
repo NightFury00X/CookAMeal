88fdc77d4fb21465e31116abc9d1e4aea14cd36d
NightFury00X <surendra136721@gmail.com>
2018-01-08 18:10:17 +0530

    Remove Sequelize logging.

--- application/controllers-services/controllers/common.controller.js
+++ application/controllers-services/controllers/common.controller.js
@@ -410,6 +410,7 @@ let Order = {
         try {
             const userId = req.user.id
             let orderDetails = req.body
+            console.log('Order: ', orderDetails)
             let recipesToJson = JSON.parse(JSON.stringify(orderDetails.recipes))
             orderDetails.user_type_id = userId
             const result = await CommonService.Order.PlaceOrder(orderDetails, recipesToJson)
