e90712a77370125590ebf790a91559aea53b0e4a
NightFury00X <surendra136721@gmail.com>
2018-01-12 12:27:27 +0530

    Updated order API.

--- application/controllers-services/controllers/common.controller.js
+++ application/controllers-services/controllers/common.controller.js
@@ -416,6 +416,7 @@ let Order = {
             const userId = req.user.id
             let recipesToJson = JSON.parse(JSON.stringify(orderData.recipes))
             const {totalAmount, taxes, deliveryFee} = orderData
+            console.log('========================', recipesToJson)
             const valid = await CommonService.Order.ValidateOrder(totalAmount, taxes, deliveryFee, recipesToJson)
             if (!valid) {
                 return ResponseHelpers.SetErrorResponse(CommonConfig.ERRORS.ORDER.FAILURE, res)
