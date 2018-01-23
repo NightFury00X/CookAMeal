afb16458d551030ebe2d45f1b08a15cb6b6d5513
NightFury00X <surendra136721@gmail.com>
2018-01-23 11:24:47 +0530

    Fixed some issues.

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ const corsOptions = {
         }
     },
     'methods': 'GET, HEAD ,PUT ,PATCH ,POST ,DELETE',
-    'preflightContinue': false,
+    'preflightContinue': true,
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials',
     'Access-Control-Allow-Credentials': true
 }
